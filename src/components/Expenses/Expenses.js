da61c9b7b6888308129df0bd95b682db9e4f9cb3
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-10 17:23:47 +0300

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpensesFilter";
 import React, { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,6 +11,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
@@ -18,14 +22,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,22 +6,22 @@ const ExpenseForm = (props) => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
   /*
-                    const [userInput, setUserInput] = useState({
-                      enteredTitle: "",
-                      enteredAmount: "",
-                      enteredDate: "",
-                    });
-                  
-                     */
+                      const [userInput, setUserInput] = useState({
+                        enteredTitle: "",
+                        enteredAmount: "",
+                        enteredDate: "",
+                      });
+
+                       */
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
     /*
-                                        setUserInput({
-                                          ...userInput,
-                                          enteredTitle: event.target.value,
-                                        });
-                                        */
+                                            setUserInput({
+                                              ...userInput,
+                                              enteredTitle: event.target.value,
+                                            });
+                                            */
   };
 
   const amountChangeHandler = (event) => {
@@ -80,6 +80,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>

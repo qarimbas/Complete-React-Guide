98edacf80141800bf92f7630c62e7bb6f45532bd
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-10 23:06:16 +0300

    Styling React Components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import styled from "styled-components";
+import React, {useState} from 'react';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
+/*
 const FormControl = styled.div`
 
     margin: 0.5rem 0;
-  
+
   & label {
     font-weight: bold;
     display: block;
@@ -29,15 +29,16 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-  
+
 `;
+ */
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
-    if(event.target.value.trim().length > 0) {
+    if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
@@ -45,7 +46,7 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    if(enteredValue.trim().length === 0) {
+    if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
@@ -53,13 +54,13 @@ const CourseInput = props => {
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
-        <label >Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
-      <Button type="submit">Add Goal</Button>
-    </form>
+      <form onSubmit={formSubmitHandler}>
+        <div className={`${styles['form-control']} ${!isValid && styles.invalid} `}>
+          <label>Course Goal</label>
+          <input type="text" onChange={goalInputChangeHandler}/>
+        </div>
+        <Button type="submit">Add Goal</Button>
+      </form>
   );
 };
 

f8a20c7caaf2322ea832d3cdf203e7286ee40c50
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-11 09:48:51 +0300

    practice project initialization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
-import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+
     </div>
   );
 }

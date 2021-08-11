26f0a4ea1e4f16e774f402ecf2626bd2be69a539
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-11 22:08:33 +0300

    wrapper added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import classes from './AddUser.module.css';
 import Button from "../UI/Button";
 import UsersList from "./UsersList";
 import ErrorModal from "../UI/errorModal";
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -44,7 +45,7 @@ const AddUser = props => {
     };
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
@@ -55,7 +56,7 @@ const AddUser = props => {
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
-        </div>
+        </Wrapper>
     );
 };
 

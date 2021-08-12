dcda3f77f9faeb12436f7aa1a017d27f003cee13
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-12 17:50:12 +0300

    custom context provider

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from "../../store/auth-context";
 
-const Navigation = (props) => {
+const Navigation = () => {
+
+    const ctx = useContext(AuthContext);
+
     return (
-        <AuthContext.Consumer>
-            {(ctx) => {
-                return <nav className={classes.nav}>
+      <nav className={classes.nav}>
                     <ul>
                         {ctx.isLoggedIn && (
                             <li>
@@ -21,15 +22,13 @@ const Navigation = (props) => {
                         )}
                         {ctx.isLoggedIn && (
                             <li>
-                                <button onClick={props.onLogout}>Logout</button>
+                                <button onClick={ctx.onLogout}>Logout</button>
                             </li>
                         )}
                     </ul>
                 </nav>
-            }}
-
-        </AuthContext.Consumer>
     );
+
 };
 
 export default Navigation;

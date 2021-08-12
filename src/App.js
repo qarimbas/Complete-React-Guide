e47473d4a409f73e082795651a2e154750f4c8aa
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-12 13:07:54 +0300

    useReducer&useEffect

--- src/App.js
+++ src/App.js
@@ -5,39 +5,37 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-      localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
         const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
-        if(storedUserLoggedInInformation === '1') {
+        if (storedUserLoggedInInformation === '1') {
             setIsLoggedIn(true);
         }
-    }, [] );
+    }, []);
 
-  const logoutHandler = () => {
-      localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <React.Fragment>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;

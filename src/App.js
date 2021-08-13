3fead707a46200866f12bd01462fef90eb1e9249
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-13 10:53:27 +0300

    food order app init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
-import './App.css';
-
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
     </div>
   );
 }

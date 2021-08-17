7d921adcfe302703afc232fe493b62f003877e90
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-17 16:46:55 +0300

    c based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
-import './App.css';
+import Users from './components/Users';
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <Users />
     </div>
   );
 }

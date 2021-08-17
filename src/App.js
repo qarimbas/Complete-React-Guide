e289a137ca4c3390c45b919e78d0078173802a7c
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-08-17 14:43:50 +0300

    lpaz init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+    <div className="app">
+      <h1>Hi there!</h1>
     </div>
   );
 }

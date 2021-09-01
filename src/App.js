a8d2c7221b5c9993e1686a4d16b2494757326485
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-01 14:38:08 +0300

    redux auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Counter from './components/Counter';
+import {Fragment} from "react";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import {useSelector} from "react-redux";
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
-  return (
-    <Counter />
-  );
+
+    const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+    return (
+        <Fragment>
+            <Header/>
+            {!isAuth && <Auth/>}
+            {isAuth && <UserProfile/>}
+            <Counter/>
+        </Fragment>
+    );
 }
 
 export default App;

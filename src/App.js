03f458201e05e484194df656d9f48ed00859f5d8
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-05 14:21:17 +0300

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
-import './App.css';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import {useSelector} from "react-redux";
 
 function App() {
+    const showCart = useSelector(state => state.ui.cartIsVisible);
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
+    <Layout>
+        {showCart &&  <Cart  />}
+      <Products />
+    </Layout>
   );
 }
 

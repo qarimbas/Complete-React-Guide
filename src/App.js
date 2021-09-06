b8741be9404bee413575c110749b7b0028654ca3
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-06 20:21:08 +0300

    init customhooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
-import './App.css';
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
   );
 }
 

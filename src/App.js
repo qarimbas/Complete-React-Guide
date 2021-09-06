b39b92b73baab8f856c854708092df5051240b7b
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-06 23:34:32 +0300

    forms init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
-import './App.css';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+    <div className="app">
+      <SimpleInput />
     </div>
   );
 }

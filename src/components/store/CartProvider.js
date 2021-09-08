8f52ccee28d49cc1eefde9a4373c44513015c910
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-08 14:41:40 +0300

    finished

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -10,9 +10,9 @@ const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     //const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
-        state.totalAmount + action.item.price * action.item.amount;
+      state.totalAmount + action.item.price * action.item.amount;
     const existingCartItemIndex = state.items.findIndex(
-        (item) => item.id === action.item.id
+      (item) => item.id === action.item.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
 
@@ -30,6 +30,26 @@ const cartReducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
 
     return {
       items: updatedItems,
@@ -37,27 +57,8 @@ const cartReducer = (state, action) => {
     };
   }
 
-  if (action.type === 'REMOVE') {
-
-
-    const existingCartItemIndex = state.items.findIndex(
-        (item) => item.id === action.id
-    );
-    const existingItem = state.items[existingCartItemIndex];
-    const updatedTotalAmount = state.totalAmount - existingItem.price;
-    let updatedItems;
-    if (existingItem.amount === 1) {
-      updatedItems = state.items.filter(item => item.id !== action.id);
-    } else {
-      const updatedItem = {...existingItem, amount: existingItem.amount - 1};
-      updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
-    }
-
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount
-    };
+  if (action.type === "CLEAR") {
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -65,16 +66,19 @@ const cartReducer = (state, action) => {
 
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
-      cartReducer,
-      defaultCartState
+    cartReducer,
+    defaultCartState
   );
 
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({type: "ADD", item: item});
+    dispatchCartAction({ type: "ADD", item: item });
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({type: "REMOVE", id: id});
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
   };
 
   const cartContext = {
@@ -82,13 +86,14 @@ const CartProvider = (props) => {
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
-      <CartContext.Provider value={cartContext}>
-        {" "}
-        {props.children}
-      </CartContext.Provider>
+    <CartContext.Provider value={cartContext}>
+      {" "}
+      {props.children}
+    </CartContext.Provider>
   );
 };
 

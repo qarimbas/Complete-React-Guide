9216cded2098a39bb229ecbac42c38d41cc3ac2d
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-09 12:50:03 +0300

    quote pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
+import {Redirect, Route, Switch} from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
 
 function App() {
   return (
-    <div>
-        <MainHeader/>
-        <header></header>
-       <main>
-           <Switch>
-               <Route path='/' exact>
-                   <Redirect to='/welcome' />
-               </Route>
-               <Route path="/welcome">
-                   <Welcome/>
-               </Route>
-               <Route path="/products" exact>
-                   <Products />
-               </Route>
-               <Route path="/products/:productId">
-                   <ProductDetail/>
-               </Route>
-           </Switch>
-       </main>
-    </div>
+      <Switch>>
+          <Route path='/' exact>
+              <Redirect to='/quotes'/>
+          </Route>
+          <Route path="/quotes" exact>
+              <AllQuotes/>
+          </Route>
+        <Route path="/quotes/:quoteId">
+            <QuoteDetail/>
+        </Route>
+        <Route path='/new-quote'>
+            <NewQuote/>
+        </Route>
+      </Switch>
+
   );
 }
 

9f2667a4f798134f7121bb488f93d72a6d7aadbb
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-09 13:28:43 +0300

    imperative navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,16 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+import {useHistory} from "react-router-dom";
+
 const NewQuote = () => {
-    return <h1>New Quote Page</h1>
+    const history = useHistory();
+
+    const addQuoteHandler = quoteData => {
+        console.log(quoteData);
+
+        history.push('/quotes');
+    };
+
+    return <QuoteForm onAddQuote={addQuoteHandler} />
 };
 
 export default NewQuote;

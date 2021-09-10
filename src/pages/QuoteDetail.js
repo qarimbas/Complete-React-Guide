60f8fedaf239ee733b9d6d7fd3d2f82d34783423
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-10 10:43:44 +0300

    firebase cpnnectedquotes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,24 +1,44 @@
-import {Route, useParams} from "react-router-dom";
-import {Fragment} from "react";
+import {Link, Route, useParams, useRouteMatch} from "react-router-dom";
+import {Fragment, useEffect} from "react";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Kerem', text: 'Learning React is fun!'},
-    {id: 'q2', author: 'Kurt', text: 'Learning React is great!'},
-];
+import useHttp from "../hooks/use-http";
+import {getSingleQuote} from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
+    const match = useRouteMatch();
     const params = useParams();
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+    const {quoteId} = params;
 
-    if(!quote) {
+    const {sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest, quoteId]);
+
+    if (status === 'pending') {
+        return <div className='centered'>
+            <LoadingSpinner />
+        </div>
+    }
+
+    if(error) {
+        return <p className='centered'>{error}</p>
+    }
+
+    if(!loadedQuote.text) {
         return <p>No quote found!</p>
     }
 
     return <Fragment>
-        <HighlightedQuote text={quote.text} author={quote.author} />
-        <Route path={`/quotes/${params.quoteId}/comments`}>
+        <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+        <Route path={match.path} exact>
+            <div className="centered"><Link className='btn--flat' to={`${match.path}/comments`} >Load Comments</Link></div>
+        </Route>
+
+        <Route path={`${match.path}/comments`}>
             <Comments />
         </Route>
     </Fragment>

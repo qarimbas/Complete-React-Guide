0b8e818496037d92f7fd065f26d545ae8f6b959a
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-16 13:06:07 +0300

    login state

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,14 @@
-import {useRef, useState} from 'react';
+import { useState, useRef, useContext } from 'react';
 
+import AuthContext from '../../store/auth-context';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -18,68 +22,81 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    //add validation
+    // optional: Add validation
+
     setIsLoading(true);
-    if(isLogin) {
-
+    let url;
+    if (isLogin) {
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAaeyywsKe4KF4NesoE0hEKayid5p2-7U0';
+    } else {
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAaeyywsKe4KF4NesoE0hEKayid5p2-7U0';
     }
-    else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAaeyywsKe4KF4NesoE0hEKayid5p2-7U0',
-          {
-            method: 'POST',
-            body: JSON.stringify({
-              email: enteredEmail,
-              password: enteredPassword,
-              returnSecureToken: true
-            }),
-            headers: {
-              'Content-Type': 'application/json'
-            }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+        .then((res) => {
+          setIsLoading(false);
+          if (res.ok) {
+            return res.json();
+          } else {
+            return res.json().then((data) => {
+              let errorMessage = 'Authentication failed!';
+              // if (data && data.error && data.error.message) {
+              //   errorMessage = data.error.message;
+              // }
+
+              throw new Error(errorMessage);
+            });
           }
-          ).then(res => {
-            setIsLoading(false);
-            if(res.ok) {
-
-            } else {
-              return res.json().then((data) => {
-                let errorMessage = 'Auth failed';
-                if(data && data.error && data.error.message)
-                {
-                  errorMessage = data.error.message;
-                }
-                alert(errorMessage);
-              });
-            }
-
-      } );
-    }
+        })
+        .then((data) => {
+          authCtx.login(data.idToken);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
   };
 
   return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required ref={emailInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required ref={passwordInputRef} />
-        </div>
-        <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
-          {isLoading && <p>Sending request...</p>}
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
+      <section className={classes.auth}>
+        <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+        <form onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <label htmlFor='email'>Your Email</label>
+            <input type='email' id='email' required ref={emailInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='password'>Your Password</label>
+            <input
+                type='password'
+                id='password'
+                required
+                ref={passwordInputRef}
+            />
+          </div>
+          <div className={classes.actions}>
+            {!isLoading && (
+                <button>{isLogin ? 'Login' : 'Create Account'}</button>
+            )}
+            {isLoading && <p>Sending request...</p>}
+            <button
+                type='button'
+                className={classes.toggle}
+                onClick={switchAuthModeHandler}
+            >
+              {isLogin ? 'Create new account' : 'Login with existing account'}
+            </button>
+          </div>
+        </form>
+      </section>
   );
 };
 

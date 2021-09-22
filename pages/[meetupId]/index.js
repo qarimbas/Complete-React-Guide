2e6d6a4aa6a1d90d77f60f9394f33b7611d04ae6
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-22 15:36:13 +0300

    getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -8,4 +8,43 @@ function MeetupDetails() {
     );
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                },
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                },
+            },
+        ],
+    };
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    console.log(meetupId);
+
+    return {
+        props: {
+            meetupData: {
+                image: "https://www.lux-review.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/03/Palma-de-Mallorca-1.jpg.webp",
+                id: meetupId,
+                title: 'First Meetup',
+                address: "La Palma Mallorca",
+                description: "This is a first meetup"
+            },
+        },
+    };
+}
+
+
 export default MeetupDetails;

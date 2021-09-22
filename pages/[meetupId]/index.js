b98ff8c7c0b20e01926a8b7ab627978c2549cb37
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-22 12:27:56 +0300

    custom omponents

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image="https://www.lux-review.com/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/03/Palma-de-Mallorca-1.jpg.webp"
+            title="First Meetup" address="La Palma Mallorca" description="This is a first meetup"/>
+    );
+}
+
+export default MeetupDetails;

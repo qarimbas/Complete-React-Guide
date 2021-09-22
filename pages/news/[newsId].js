421cb1449fc67509b20f8d5c62602c91440d5217
B. Kerem KURT <Kerem.Kurt@adesso.com.tr>
2021-09-22 11:32:06 +0300

    link btw pages

---
+++ pages/news/[newsId].js
@@ -0,0 +1,16 @@
+import {useRouter} from "next/router";
+
+
+//our-domain/mews
+
+function DetailPage() {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+
+
+    return <h1>The Detail Page</h1>
+}
+
+export default DetailPage;

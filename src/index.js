60755aa7e10f32d1523e751fb6989283c80d9280
s21kkurt <kkurt@sabanciuniv.edu>
2021-10-15 21:17:44 +0300

    Context API

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import ProductsProvider from './context/products-context';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
-
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+    <ProductsProvider>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </ProductsProvider>,
+    document.getElementById('root')
 );

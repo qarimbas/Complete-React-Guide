951c16358932c299781ad96304587abc01172efc
s21kkurt <kkurt@sabanciuniv.edu>
2021-10-15 20:33:41 +0300

    list anims

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './List.css';
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 class List extends Component {
     state = {
@@ -23,21 +24,25 @@ class List extends Component {
         });
     }
 
-    render () {
-        const listItems = this.state.items.map( (item, index) => (
-            <li 
-                key={index}
-                className="ListItem" 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+    render() {
+        const listItems = this.state.items.map((item, index) => (
+            <CSSTransition key={index} classNames="fade" timeout={300}>
+                <li
+                    className="ListItem"
+                    onClick={() => this.removeItemHandler(index)}>
+                    {item}
+                </li>
+            </CSSTransition>
+
+        ));
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
+                <TransitionGroup className={"List"} component={"ul"}>
                     {listItems}
-                </ul>
+                </TransitionGroup>
             </div>
         );
     }

4c642eb1e5817dd7d06ecae96b74814d0ed0a0a4
s21kkurt <kkurt@sabanciuniv.edu>
2021-10-15 20:18:11 +0300

    CSS classNames

--- src/App.js
+++ src/App.js
@@ -30,8 +30,17 @@ class App extends Component {
                 </button>
                 <br/>
 
-                <Transition in={this.state.showBlock} timeout={300}
-                            mountOnEnter unmountOnExit
+                <Transition
+                    in={this.state.showBlock}
+                    timeout={300}
+                    mountOnEnter
+                    unmountOnExit
+                    onEnter={() => console.log('onEnter')}
+                    onEntering={() => console.log('onEntering')}
+                    onEntered={() => console.log('onEntered')}
+                    onExit={() => console.log('onExit')}
+                    onExiting={() => console.log('onExiting')}
+                    onExited={() => console.log('onExited')}
                 >
                     {state => (
                         <div
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './Modal.css';
-import {Transition} from "react-transition-group";
+import {CSSTransition} from "react-transition-group";
 
 const animationTiming = {
     enter: 400,
@@ -12,23 +12,20 @@ const modal = (props) => {
 
 
     return (
-        <Transition
+        <CSSTransition
             mountOnEnter
             unmountOnExit
             in={props.show}
-            timeout={animationTiming}>
-            {state => {
-                const cssClasses = ["Modal",
-                    state === 'entering' ? 'ModalOpen' : state === 'exiting' ? 'ModalClosed' : null
-                ];
-                return (
-                    <div className={cssClasses.join(' ')}>
-                        <h1>A Modal</h1>
-                        <button className="Button" onClick={props.closed}>Dismiss</button>
-                    </div>
-                );
-            }}
-        </Transition>
+            timeout={animationTiming}
+            classNames="fade-slide">
+
+            <div className="Modal">
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+            </div>
+
+
+        </CSSTransition>
     );
 
 };

26a1bf5f661f9fe2bca9a35077d0abeef9983f42
s21kkurt <kkurt@sabanciuniv.edu>
2021-10-15 18:50:02 +0300

    Transition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,7 +6,8 @@ const modal = (props) => {
 
     const cssClasses = [
         "Modal",
-        props.show ? "ModalOpen" : "ModalClosed"
+        props.show === 'entering' ? "ModalOpen" 
+        : props.show === 'exiting' ? 'ModalClosed' : null
     ];
 
     return(

59da3cf45024f994ee095fcb6d5d7a1c52a11a98
s21kkurt <kkurt@sabanciuniv.edu>
2021-10-16 21:36:03 +0300

    async test

---
+++ src/components/Aysnc.test.js
@@ -0,0 +1,11 @@
+import Async from "./Async";
+import {render, screen} from "@testing-library/react";
+
+describe('Async component', () => {
+    test('renders posts if request succeeds', async () => {
+        render(<Async/>)
+
+        const listItemElements = await screen.findAllByRole('listitem');
+        expect(listItemElements).not.toHaveLength(0);
+    });
+});

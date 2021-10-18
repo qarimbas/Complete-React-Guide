cbeb2ec973a0daf72967dec1252902b133b998fa
s21kkurt <kkurt@sabanciuniv.edu>
2021-10-18 16:05:00 +0300

    remove Todo

--- react-ts/src/components/TodoItem.tsx
+++ react-ts/src/components/TodoItem.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import classes from './TodoItem.module.css';
 
-const TodoItem: React.FC<{text: string}> = (props) => {
-    return <li className={classes.item}>{props.text}</li>
+const TodoItem: React.FC<{text: string; onRemoveTodo: (event: React.MouseEvent) => void}> = (props) => {
+    return <li onClick={props.onRemoveTodo} className={classes.item}>{props.text}</li>
 };
 
 export default TodoItem;
